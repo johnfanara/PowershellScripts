2a8daa87498759adfe969b2e949c834cde5163fe
johnfanara <64872359+johnfanara@users.noreply.github.com>
2024-12-14 17:03:12 -0500

    Script to save results of Wireshark capture

---
+++ WiresharkCapture.ps1
@@ -0,0 +1,14 @@
+﻿$tempCaptureFile = [System.IO.Path]::GetTempFileName() + ".pcap"
+
+$tsharkArgs = "-i 5 -w $tempCaptureFile"
+Start-Process -FilePath "C:\Program Files\Wireshark\tshark.exe" -ArgumentList $tsharkArgs -NoNewWindow
+
+Write-Host "Capture is running. Press Enter to stop the capture"
+
+Read-Host
+
+Stop-Process -Name tshark
+
+Write-Host "Capture saved to: $tempCaptureFile"
+
+Start-Process -FilePath "C:\Program Files\Wireshark\wireshark.exe" -ArgumentList $tempCaptureFile
